20bb9413daf0104462d7a2ef0a88cfae38cbee5f
Muneeza Saif <muneezasaiff@gmail.com>
2026-09-08 20:12:08 +0500

    Lab4

---
+++ src/main/java/com/university/lab/lab4task1/Lab4task1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.university.lab.lab4task1;
+
+/**
+ *
+ * @author natas
+ */
+public class Lab4task1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
